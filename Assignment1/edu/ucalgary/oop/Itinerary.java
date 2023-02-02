0c7d77989ddddb3a98684179e3385d9767d8e078
Tochiisaac <114114023+Tochiisaac@users.noreply.github.com>
2023-02-02 16:05:57 -0700

    Update Itinerary.java

--- Assignment1/edu/ucalgary/oop/Itinerary.java
+++ Assignment1/edu/ucalgary/oop/Itinerary.java
@@ -11,23 +11,68 @@ public class Itinerary {
     // Returns a string in the format of:
     // value (key)
     public static String fmtString(String key, String value) {
+        return value + " (" + key + ")";
     }
 
     // Constructor
     public Itinerary(String[][] myTrips) {
+        for (int i = 0; i < myTrips.length; i++) {
+            trips[i] = new Trip(myTrips[i]);
+        }
     }
 
     // Getter
     public Trip[] getTrips() {
+        return this.trips;
     }
 
     public String formatByArrival() {
+        StringBuilder sb = new StringBuilder();
+        int currYear = 0, currMonth = 0;
+        for (Trip trip : trips) {
+            if (trip == null) break;
+            int year = trip.getYear(trip.getArrival());
+            int month = trip.getMonth(trip.getArrival());
+            if (year != currYear) {
+                sb.append(year + " (Year)\n");
+                currYear = year;
+                currMonth = 0;
+            }
+            if (month != currMonth) {
+                sb.append("--" + month + " (Month)\n");
+                currMonth = month;
+            }
+            sb.append("----" + trip.getCity() + " (City), " + trip.getCountry() + " (Country) (Place)\n");
+        }
+        return sb.toString().strip();
     }
 
     // The first array holds years (2021-2023).
     // The second array holds months.
     // The third array holds formatted locations occurring in the year/month
-    String[][][] byDate() {
+    public String[][][] byDate() {
+        String[][][] dateArray = new String[3][12][20];
+        int count = 0;
+        for (Trip trip : trips) {
+            if (trip == null) break;
+            int year = trip.getYear(trip.getArrival()) - 2021;
+            int month = trip.getMonth(trip.getArrival()) - 1;
+            boolean added = false;
+            for (int i = 0; i < 20; i++) {
+                if (dateArray[year][month][i] == null) {
+                    dateArray[year][month][i] = fmtString("City", trip.getCity()) + ", " + fmtString("Country", trip.getCountry());
+                    added = true;
+                    break;
+                }
+            }
+            if (!added) {
+                count++;
+            }
+        }
+        return dateArray;
     }
 
+
 }
+
+
